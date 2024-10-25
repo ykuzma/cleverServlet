14de83390e9af59d6a489bf19e573c6520d5c1eb
Yury Kuzma <vterkin1388@gmail.com>
2024-10-25 21:24:08 +0300

    add in UserServlet dependence from UserService

--- api/src/main/java/by/kuzma/servlets/UserServlet.java
+++ api/src/main/java/by/kuzma/servlets/UserServlet.java
@@ -1,6 +1,6 @@
 package by.kuzma.servlets;
 
-import jakarta.servlet.ServletConfig;
+import by.kuzma.service.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,28 +10,29 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet(name = "user", urlPatterns = "/user")
 public class UserServlet extends HttpServlet {
-    private final Map<String, String> users = new HashMap<>();
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        users.put("User", "User 42");
+    private UserService userService;
+
+
+    public UserServlet() {
+
     }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         Object sessionAttribute;
         PrintWriter writer = resp.getWriter();
-        if(session != null && (sessionAttribute = session.getAttribute("userName")) != null ) {
-            writer.print(users.get(sessionAttribute.toString()));
-        }else {
-            writer.print("user not found");
+        if (session != null && (sessionAttribute = session.getAttribute("userName")) != null) {
+            resp.setStatus(200);
+            writer.print(userService.getUserByLogin(sessionAttribute.toString()));
+        } else {
+            resp.setStatus(404);
+            writer.print(resp.getStatus() + "  user not found");
         }
 
     }
