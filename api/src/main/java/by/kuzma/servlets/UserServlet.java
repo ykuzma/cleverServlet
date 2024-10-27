7704fd7b1db4eac7f3e72ab3f0cb625690c54915
Yury Kuzma <vterkin1388@gmail.com>
2024-10-27 20:38:38 +0300

    refactor userServlet

--- api/src/main/java/by/kuzma/servlets/UserServlet.java
+++ api/src/main/java/by/kuzma/servlets/UserServlet.java
@@ -1,6 +1,7 @@
 package by.kuzma.servlets;
 
 import by.kuzma.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,26 +10,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "user", urlPatterns = "/user")
 public class UserServlet extends HttpServlet {
-
     private transient UserService userService;
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        Object sessionAttribute;
-        PrintWriter writer = resp.getWriter();
-        if (session != null && (sessionAttribute = session.getAttribute("userName")) != null) {
-            resp.setStatus(200);
-            writer.print(userService.getUserByLogin(sessionAttribute.toString()));
-        } else {
-            resp.setStatus(404);
-            writer.print(resp.getStatus() + "  user not found");
-        }
+        String userName = session.getAttribute("userName").toString();
+        resp.getWriter().print(new ObjectMapper().writeValueAsString(userService.getUserByLogin(userName)));
+        resp.setStatus(HttpServletResponse.SC_OK);
+
 
     }
 }
